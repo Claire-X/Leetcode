b9f377f0842cb814977c7f3776201c5ae27f7054
Zixuan QU <qzx1107@gmail.com>
2015-02-06 19:51:48 +0100

    Implement strStr()

--- Code/strStr.cpp
+++ Code/strStr.cpp
@@ -1,8 +1,38 @@
 class Solution {
 public:
-    int strStr(char *haystack, char *needle) {
-        string str(haystack);
-        string s(needle);
-        return str.find(s);
-    }
+	int strStr(char *haystack, char *needle) {
+		if (!(*needle))
+			return 0;
+
+		int i = 0, j = 0;
+		int index = -1,tmp;
+		while (haystack[i]) {
+			if (haystack[i] != needle[j]) {
+				i++;
+			} else {
+				index = i;
+				while (haystack[i] && needle[j]) {
+					if (haystack[i] != needle[j]) {
+						j = 0;
+						i=index+1;
+						index = -1;
+						break;
+					} else {
+						i++;
+						j++;
+					}
+				}
+
+				if (!needle[j]) {
+					return index;
+				} else if (!haystack[i]) {
+					index = -1;
+				}
+			}
+
+		}
+
+		return index;
+
+	}
 };
