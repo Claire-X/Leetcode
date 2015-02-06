64fd36638ad140f5efbf9a27c587242202ec46e1
Zixuan QU <qzx1107@gmail.com>
2015-02-06 15:49:43 +0100

    add binary

---
+++ Code/addBinary.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+	string addBinary(string a, string b) {
+
+		if (a.size() < b.size()) {
+			string c(b.size() - a.size(), '0');
+			c.append(a);
+			a = c;
+		}
+		if (b.size() < a.size()) {
+			string c(a.size() - b.size(), '0');
+			c.append(b);
+			b = c;
+		}
+
+		auto ia = a.rbegin();
+		auto ib = b.rbegin();
+
+		string sum = "";
+		int carry = 0;
+		while (ia != a.rend()) {
+			int ai = (*ia) - '0';
+			int bi = (*ib) - '0';
+			int val = (ai + bi + carry) % 2;
+			//cout<<val<<endl;
+			sum += ('0' + val);
+			carry = (ai + bi + carry) / 2;
+			//cout<<carry<<endl;
+			ia++;
+			ib++;
+		}
+		if (carry)
+			sum += '1';
+		reverse(sum.begin(), sum.end());
+		return sum;
+
+	}
+};
