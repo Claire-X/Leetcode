83186d9082e459fa8e0500b568b02ef06b6bafc8
Zixuan QU <qzx1107@gmail.com>
2015-02-06 14:59:19 +0100

    Merge Two Sorted Lists

--- Code/mergeList.cpp
+++ Code/mergeList.cpp
@@ -1,11 +1,8 @@
 class Solution {
 public:
     ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
-        if(l1==nullptr) return l2;
-        if(l2==nullptr) return l1;
-        
-        ListNode head(0);
-        ListNode *pre=&head;
+        ListNode *pre,*head=new ListNode(0);
+        pre=head;
         
          
         while(l1&&l2){
@@ -13,17 +10,20 @@ public:
                 pre->next=l1;
                 pre=pre->next;
                 l1=l1->next;
+                
             }
             else{
                 pre->next=l2;
                 pre=pre->next;
                 l2=l2->next;
+                
             } 
+               
         }
         
         if(l1) pre->next=l1;
         if(l2) pre->next=l2;
         
-        return head.next;
+        return head->next;
     }
 };
