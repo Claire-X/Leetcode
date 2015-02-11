26fba5c772f35041065d2f1bb8a30aaf2aadc646
Zixuan QU <qzx1107@gmail.com>
2015-02-11 23:15:43 +0100

    Letter Combinations of a Phone Number

---
+++ Code/letterCombinations.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<string> letterCombinations(string digits) {
+        if(digits.size()==0) return vector<string>(1,"");
+    vector<string> touche({" ","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"});
+    vector<string> current,next;
+    for(size_t i=0;i<digits.size();i++){
+       if(i == 0){
+           for(auto c:touche[digits[i]-'0'])
+               current.push_back(string(1,c));
+       }
+       else{
+           for(char c:touche[digits[i]-'0'])
+               for(auto s : current){
+                   next.push_back(s+=c);
+               }
+           if(i==current.size()-1) return current;
+           current.clear();
+           swap(current,next);
+       }
+    }
+    return current;
+    }
+};
