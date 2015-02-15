91e82d9854435e870821d009072465d50248273a
Zixuan QU <qzx1107@gmail.com>
2015-02-16 00:41:15 +0100

    Binary Tree Preorder Traversal

---
+++ Code/preorder.cpp
@@ -0,0 +1,34 @@
+//recursion
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode *root) {
+        vector<int> result;
+        return pre(root,result);
+    }
+    vector<int> pre(TreeNode* root, vector<int> & result){
+        if(!root) return result;
+        result.push_back(root->val);
+        pre(root->left,result);
+        pre(root->right,result);
+        return result;
+    }
+};
+//iteration
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode *root) {
+        vector<int> result;
+        if(!root) return result;
+        stack<TreeNode*> stk;
+        stk.push(root);
+        while(!stk.empty()){
+            TreeNode *n = stk.top();
+            stk.pop();
+            result.push_back(n->val);
+            if(n->right) stk.push(n->right);
+            if(n->left) stk.push(n->left);
+        }
+        return result;
+        
+    }
+};
