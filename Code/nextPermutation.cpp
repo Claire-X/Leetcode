66ade0f57f7902bf6074318923afe47420cf34ea
Zixuan QU <qzx1107@gmail.com>
2015-02-16 13:05:48 +0100

    Next Permutation

--- Code/nextPermutation.cpp
+++ Code/nextPermutation.cpp
@@ -1,3 +1,4 @@
+//O(n2)
 class Solution {
 public:
 	void nextPermutation(vector<int> &num) {
@@ -22,4 +23,30 @@ public:
 		reverse(num.begin(), num.end());
 		return;
 	}
+};
+
+//O(n)
+class Solution {
+public:
+    void nextPermutation(vector<int> &num) {
+
+        int l=-1,r=-1;
+        for(int i =num.size()-1;i>0;i--)
+            if(num[i-1]<num[i]){
+                l = i-1;
+                break;
+            }
+        for(int j = l+1;j<num.size();j++){
+            if(num[j]>num[l])
+                r = j;
+        }           
+        if(l!=-1 && r!=-1) {
+            swap(num[l],num[r]);
+            sort(num.begin()+l+1,num.end());
+            return;
+        }
+
+        reverse(num.begin(),num.end());
+        return;
+    }
 };
