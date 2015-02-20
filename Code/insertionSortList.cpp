cb4a8cb68c0d89d23ba2c16b44f7949c09b0e7f1
Zixuan QU <qzx1107@gmail.com>
2015-02-20 17:52:37 +0100

    Insertion Sort List

---
+++ Code/insertionSortList.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    ListNode *insertionSortList(ListNode *head) {
+        if(!head || !head->next) return head;
+        ListNode dummy(0);
+        dummy.next = head;
+        ListNode *prev = head,*p = head->next;
+        int curlen = 1;
+        while(p){
+            ListNode* it = dummy.next,*pre = &dummy;
+            for(int i = 0;i<curlen;i++){
+                if(p->val < it->val) break;
+                pre = it;
+                it = it->next;
+            }
+            if(it != p){
+                pre->next = p;
+                prev->next = p->next;
+                p->next = it;
+                p = prev->next;
+            }else{
+                prev = p;
+                p= p->next;
+            }
+            curlen++;
+        }
+        return dummy.next;
+    }
+};
