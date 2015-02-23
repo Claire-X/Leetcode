d3805b806970f72681429c870e5592c2b5b85eb8
Zixuan QU <qzx1107@gmail.com>
2015-02-23 21:11:41 +0100

    Combination Sum II

--- Code/combinationSum2.cpp
+++ Code/combinationSum2.cpp
@@ -10,13 +10,14 @@ public:
 private:
     void dfs(vector<int> &candidates, vector<vector<int> > &result,vector<int> &path,int pos,int gap){
         if(gap == 0) {
-             if(find(result.begin(),result.end(),path)==result.end())
                 result.push_back(path);
             return;
          }
-         
+         int pre=-1;//make sure that the elements with the same value will be used only once
          for(int i = pos;i<candidates.size();i++){
              if(gap<candidates[i]) return;
+             if(pre == candidates[i]) continue;
+             pre = candidates[i];
              path.push_back(candidates[i]);
              dfs(candidates,result,path,i+1,gap-candidates[i]);
              path.pop_back();
