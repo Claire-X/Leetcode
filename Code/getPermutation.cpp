8e728d5eb9de0536f3cfdc1596012c24fdab0e86
Zixuan QU <qzx1107@gmail.com>
2015-02-23 17:23:46 +0100

    Permutation Sequence

--- Code/getPermutation.cpp
+++ Code/getPermutation.cpp
@@ -1,3 +1,34 @@
+//cantor unfold
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+        if(n==1) return "1";
+        vector<int> select(n);
+        iota(select.begin(),select.end(),1);
+        string result;
+        k--;
+        while(n){
+
+            int digit = k/factor(n-1);
+            k%=factor(n-1);
+            
+            result+=*next(select.begin(),digit)+'0';
+            select.erase(next(select.begin(),digit));
+            n--;
+        }
+        return result;
+    }
+
+    int factor(int n){
+        int sum = 1,i=1;
+        while(++i<=n)
+           sum*=i;
+        return sum;
+    }
+};
+
+
+//math
 class Solution {
 public:
 	string getPermutation(int n, int k) {
@@ -7,7 +38,7 @@ public:
 		string result;
 		while (n) {
 
-			int digit = k % factor(n - 1) ? k / factor(n - 1) + 1 : k / factor(n - 1);
+			int digit =  (k-1) / factor(n - 1);
 			k %= factor(n - 1);
 			int count = 0;
 			for (int i = 0; i < select.size(); i++) {
