c3f6a325c3b27365e1d8b6dce5e4ee2a098fe69e
Zixuan QU <qzx1107@gmail.com>
2015-03-04 00:25:17 +0100

    Median of Two Sorted Arrays

--- Code/findMedianSortedArrays.cpp
+++ Code/findMedianSortedArrays.cpp
@@ -1,3 +1,50 @@
+//第二次写借鉴quickSort的方法
+class Solution {
+public:
+    double findMedianSortedArrays(int A[], int m, int B[], int n) {
+       if(m+n==1) return m>0?A[0]:B[0];
+       bool flag = (m+n)%2;
+       int k =(m+n)/2,i=0,j=0,left,right;
+       while(i+j<k+1){
+           int a = i<m?A[i]:INT_MAX;
+           int b = j<n?B[j]:INT_MAX;
+           if(a<b) {
+               i++;
+               if(i+j==k) left=a;
+               if(i+j ==k+1) right =a;
+           }
+           else {
+               j++;
+               if((i+j)==k) left=b;
+               if((i+j)== k+1) right =b;
+           }
+      }
+        return flag? right:(left+right)/2.0;
+    }
+
+};
+
+//O(log(m+n))的方法
+class Solution {
+public:
+    double findMedianSortedArrays(int A[], int m, int B[], int n) {
+       int k = (m+n)/2;
+       return (m+n)%2? findK(A,m,B,n,k+1):(findK(A,m,B,n,k+1)+findK(A,m,B,n,k))/2.0;
+    }
+    double findK(int A[], int m, int B[], int n,int k){
+        if(m>n) return findK(B,n,A,m,k);
+        if(m==0) return B[k-1];
+        if(k==1) return min(A[0],B[0]);
+        int a = k/2>m? m:k/2-1;
+        int b = k-a;
+        if(B[b-1]<A[a-1]) return findK(A,m,B+b,n-b,k-b);
+        if(B[b-1]>A[a-1]) return findK(A+a,m-a,B,n,k-a);
+        else return A[a-1];
+    }
+
+};
+
+//第一次写，代码过于冗余
 class Solution {
 public:
 	double findMedianSortedArrays(int A[], int m, int B[], int n) {
