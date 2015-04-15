d3cc034a0f79eed7fea3c9aadcdcb6b1feafb020
Zixuan QU <qzx1107@gmail.com>
2015-04-15 23:22:45 +0200

    Symmetric Tree

--- Code/isSymmetric.cpp
+++ Code/isSymmetric.cpp
@@ -24,3 +24,19 @@ public:
         return true;
     }
 };
+
+//recursion
+class Solution {
+public:
+    bool isSymmetric(TreeNode *root) {
+        if(!root) return true;
+        return isSym(root->left,root->right);
+    }
+    
+    bool isSym(TreeNode *a,TreeNode *b){
+        if(!a && !b ) return true;
+        if(!a||!b) return false;
+  
+        return (a->val == b->val) && isSym(a->left,b->right) && isSym(a->right,b->left);
+    }
+};
