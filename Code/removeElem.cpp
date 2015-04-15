81d6c89e889b8a21b09c166c11c9e58b5a7455f8
Zixuan QU <qzx1107@gmail.com>
2015-04-16 00:54:28 +0200

    Remove Element

--- Code/removeElem.cpp
+++ Code/removeElem.cpp
@@ -1,3 +1,15 @@
+//similar to remove duplicates
+class Solution {
+public:
+    int removeElement(int A[], int n, int elem) {
+        if (!n) return 0;
+        int index = 0;
+        for(int i =0;i<n;i++){
+            if(A[i]!=elem) A[index++] = A[i];
+        }
+        return index;
+    }
+};
 //second
 class Solution {
 public:
