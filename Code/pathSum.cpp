381c0a002e26cf8fba6d73a26b91274bb94bf425
Zixuan QU <qzx1107@gmail.com>
2015-04-15 18:27:27 +0200

    Path Sum

--- Code/pathSum.cpp
+++ Code/pathSum.cpp
@@ -1,9 +1,53 @@
 /*
  * pathSum.cpp
  *
- *  Created on: 2015Äê1ÔÂ31ÈÕ
+ *  Created on: 2015Ã„Ãª1Ã”Ã‚31ÃˆÃ•
  *      Author: Claire
  */
+ //recursion
+ class Solution {
+public:
+    bool hasPathSum(TreeNode *root, int sum) {
+        if(!root) return false;
+        if((root->val==sum) && (!root->left) && (!root->right)) return true;
+
+        
+       if(root->left) root->left->val+=root->val;
+       if(root->right) root->right->val+=root->val;
+       
+       return hasPathSum(root->left,sum)||hasPathSum(root->right,sum);
+       
+    }
+};
+ //second
+ class Solution {
+public:
+    bool hasPathSum(TreeNode *root, int sum) {
+        if(!root) return false;
+
+        queue<TreeNode*> q;
+        q.push(root);
+        
+        while(!q.empty()){
+            TreeNode* n = q.front();
+            q.pop();
+            if((n->val==sum) && (!n->left) && (!n->right)) return true;
+            if(n->left) {
+                n->left->val+=n->val;
+                q.push(n->left);
+            }
+            if(n->right) {
+                n->right->val += n->val;
+                q.push(n->right);
+            }
+        }
+        
+        return false;
+       
+    }
+};
+ 
+ //first
 class Solution {
 public:
     bool hasPathSum(TreeNode *root, int sum) {
