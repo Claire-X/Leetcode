539947f5293fe7959e9d14c26e0f440e32684051
Zixuan QU <qzx1107@gmail.com>
2015-04-16 16:28:52 +0200

    Restore IP Addresses

--- Code/restoreIPAddresses.cpp
+++ Code/restoreIPAddresses.cpp
@@ -1,3 +1,77 @@
+//modification of first
+class Solution {
+public:
+	vector<string> restoreIpAddresses(string s) {
+		vector<string> result;
+		if (s.length() < 4) return result;
+		string cur;
+		dfs(s, cur, 0, 1,1, result);
+		return result;
+	}
+private:
+	void dfs(string& s, string cur, int pre, int end, int step,vector<string> &result) {
+		
+		if (pre == s.length()) {
+			if (step == 5) {
+				cur.pop_back();
+				result.push_back(cur);
+				return;
+			} else return;
+		}
+		
+		if (end - pre > 3||end>s.length()||step==5)
+			return;
+			
+		if(s[pre]!='0') dfs(s, cur, pre, end + 1,step, result);
+		
+		if (stoi(s.substr(pre, end - pre)) <= 255) {
+			cur += s.substr(pre, end - pre);
+			cur+= '.';
+			dfs(s, cur, end, end + 1, step+1,result);
+		}
+	}
+};
+//second
+class Solution {
+public:
+    vector<string> restoreIpAddresses(string s) {
+        vector<string> result;
+        if(s.size()<4) return result;
+        string cur;
+        dfs(result,cur,s,0,1);
+        return result;
+    }
+    
+    void dfs(vector<string> &result,string cur,string &s,int i,int step){
+        if(i==s.size()&&step==5){
+            cur.pop_back();
+            result.push_back(cur);
+            return;
+        } else if(i==s.size()||step==5) return;
+        
+        string tmp = cur;
+        
+        cur+=s[i];
+        cur+='.';
+        dfs(result,cur,s,i+1,step+1);
+        if(s[i]=='0') return;
+        
+        if(s.size()-i>=2 && stoi(s.substr(i,2))<256) {
+            cur=tmp;
+            cur+=s.substr(i,2);
+            cur+='.';
+            dfs(result,cur,s,i+2,step+1);
+        }
+        if(s.size()-i>=3 && stoi(s.substr(i,3))<256) {
+            cur=tmp;
+            cur+=s.substr(i,3);
+            cur+='.';
+            dfs(result,cur,s,i+3,step+1);
+        }
+        
+    }
+};
+//first
 class Solution {
 public:
 	vector<string> restoreIpAddresses(string s) {
