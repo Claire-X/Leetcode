172b100d80c01f9bfce80ff539730cb93815f91b
Zixuan QU <qzx1107@gmail.com>
2015-04-17 12:48:10 +0200

    Bitwise AND of Numbers Range

--- Code/rangeBitwiseAnd.cpp
+++ Code/rangeBitwiseAnd.cpp
@@ -1,4 +1,20 @@
-//
+//we analyse the problem by binary representation of m and n, if the number of bits of n is greater than m, than bitwise And must
+//be 0; secondly, the unequal digits at the tail of m and n result in 0s in the bitwise And. So the problem turns into looking for
+//the common parts at the beginning of m and n if their digits are equal.
+//simplified
+class Solution {
+public:
+    int rangeBitwiseAnd(int m, int n) {
+        int i = 0;
+        while (m != n) {
+            n >>= 1;
+            m >>= 1;
+            ++i;
+        }
+        return (m << i);
+    }
+};
+//my solution
 class Solution {
 public:
     int rangeBitwiseAnd(int m, int n) {
