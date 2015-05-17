a36ad688ce559235c98f28e0024a5c425f26384e
Zixuan QU <qzx1107@gmail.com>
2015-05-17 14:06:24 +0200

    Flatten Binary Tree to Linked List

--- Code/flatten.cpp
+++ Code/flatten.cpp
@@ -1,3 +1,32 @@
+//second
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        if(!root) return;
+        fla(root);
+    }
+    
+    TreeNode* fla(TreeNode* root){
+
+        TreeNode* l=root->left,*r=root->right;
+        if(!l&&!r) return root;
+        if(l&&r){
+            root->left=NULL;
+            root->right = l;
+            fla(l)->right = r;
+            return fla(r);
+        }
+        if(!l&&r){
+            return fla(r);
+        }
+        if(l&&!r){
+            root->left = NULL;
+            root->right = l;
+            return fla(l);
+        }
+    }
+};
+//first
 class Solution {
 public:
 	void flatten(TreeNode *root) {
