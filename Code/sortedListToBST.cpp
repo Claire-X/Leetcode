75e8239321b6a9ace7e10e980dd0dfa6d3ca9688
Zixuan QU <qzx1107@gmail.com>
2015-05-17 16:44:46 +0200

    Convert Sorted List to Binary Search Tree

--- Code/sortedListToBST.cpp
+++ Code/sortedListToBST.cpp
@@ -1,3 +1,40 @@
+//second 24ms
+class Solution {
+public:
+    TreeNode* sortedListToBST(ListNode* head) {
+        return convert(head,len(head));
+    }
+    
+    TreeNode* convert(ListNode*head, int l){
+        if(!head) return NULL;
+        if(!head->next) return new TreeNode(head->val);
+        
+        int ll = l/2;
+        ListNode *tmp=head;
+        while(--ll)
+            head = head->next;
+
+        ListNode *r=head->next;
+        head->next = NULL;
+        
+        TreeNode *root = new TreeNode(r->val);
+        root->right = convert(r->next,l-l/2-1);
+        root->left = convert(tmp,l/2);
+        
+        return root;
+    }
+    
+    int len(ListNode* head){
+        int l=0;
+        while(head){
+            l++;
+            head=head->next;
+        }
+        return l;
+    }
+};
+
+//first
 class Solution {
 public:
 	TreeNode *sortedListToBST(ListNode *head) {
