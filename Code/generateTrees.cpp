f87a76e1f3cc9140585a0aff5938164bd00e4b3f
Zixuan QU <qzx1107@gmail.com>
2015-05-17 23:45:43 +0200

    Unique Binary Search Trees II

--- Code/generateTrees.cpp
+++ Code/generateTrees.cpp
@@ -1,3 +1,29 @@
+//second
+class Solution {
+public:
+    vector<TreeNode*> generateTrees(int n) {
+       return generate(n,1); 
+    }
+    vector<TreeNode*> generate(int n, int start){
+        if(n==0) return vector<TreeNode*>{nullptr};
+        if(n==1) return vector<TreeNode*>{new TreeNode(start)};
+        vector<TreeNode*> result;
+        for(int i=1;i<=n;i++){
+            vector<TreeNode*> left = generate(i-1,start);
+            vector<TreeNode*> right = generate(n-i,start+i);
+            for(auto l:left)
+                for(auto r:right){
+                    TreeNode* root = new TreeNode(start+i-1);
+                    root->left = l;
+                    root->right = r;
+                    result.push_back(root);
+                }
+        }
+        return result;
+            
+    }
+};
+//first
 class Solution {
 public:
 	vector<TreeNode *> generateTrees(int n) {
