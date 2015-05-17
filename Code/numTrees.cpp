ffc70b7a98e796a7b0777bd5bfae37cb2608e577
Zixuan QU <qzx1107@gmail.com>
2015-05-17 22:58:48 +0200

    Unique Binary Search Trees

--- Code/numTrees.cpp
+++ Code/numTrees.cpp
@@ -1,3 +1,19 @@
+//second
+class Solution {
+public:
+    int numTrees(int n) {    
+        int f[n+1];
+        fill_n(f+2,n-1,0);
+        f[0]=f[1]=1;
+        
+        for(int i = 2;i<=n;i++)
+            for(int j=1;j<=i;j++)
+                f[i]+=f[j-1]*f[i-j];
+        
+        return f[n];
+    }
+};
+//first
 class Solution {
 public:
 	int numTrees(int n) {
