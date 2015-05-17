d0e5a228a59babc89064ecd48755a25db53e535f
Zixuan QU <qzx1107@gmail.com>
2015-05-17 14:31:39 +0200

    Path Sum II

--- Code/pathSum2.cpp
+++ Code/pathSum2.cpp
@@ -1,3 +1,32 @@
+//second
+class Solution {
+public:
+    vector<vector<int>> pathSum(TreeNode* root, int sum) {
+        vector<vector<int> > result;
+        if(!root) return result;
+        vector<int> path;
+        dfs(root,path,result,sum);
+        return result;
+         
+    }
+    void dfs(TreeNode *root,vector<int> &path,vector<vector<int>> &result,int sum){
+        path.push_back(root->val);
+        sum-=root->val;
+        if(!root->right&&!root->left){
+            if(!sum) result.push_back(path);
+            path.pop_back();
+            return;
+        }
+        if(root->left)
+            dfs(root->left,path,result,sum);
+        
+        if(root->right)
+            dfs(root->right,path,result,sum);
+        
+        path.pop_back();
+    }
+};
+//first
 class Solution {
 public:
     vector<vector<int> > pathSum(TreeNode *root, int sum) {
