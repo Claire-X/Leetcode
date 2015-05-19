1fd017c796560ec78bf6a76da8d46ec527c14e93
Zixuan QU <qzx1107@gmail.com>
2015-05-19 14:12:55 +0200

    Partition List

--- Code/partition.cpp
+++ Code/partition.cpp
@@ -1,3 +1,28 @@
+//second
+// anchor means the first element that is greater than or equal to x. After anchor is found in the later loop,
+//each time we found an element that is less than x, we insert it after the anchor, and move anchor by one step.
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        ListNode dummy(0),*pre = &dummy,*anchor=NULL;
+        dummy.next = head;
+        for(;head;pre=head,head=head->next){
+            if(!anchor){
+                if(head->val>=x) 
+                    anchor = pre;
+                else continue;
+            }else if(head->val<x){
+                    pre->next = head->next;
+                    head->next = anchor->next;
+                    anchor->next = head;
+                    anchor = anchor->next;
+                    head = pre;
+            }
+        }
+        return dummy.next;
+    }
+};
+//first reference to Leetcode详解
 class Solution {
 public:
     ListNode *partition(ListNode *head, int x) {
