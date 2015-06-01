9f17d921aa21e6ab8898ed2bcebcdf1fa44b5494
Zixuan QU <qzx1107@gmail.com>
2015-06-01 10:11:13 +0200

    Contains Duplicate II

--- Code/duplicate2.cpp
+++ Code/duplicate2.cpp
@@ -4,10 +4,8 @@ public:
         unordered_map<int,int> umap;
         int md=INT_MAX;
         for(int i =0;i<nums.size();i++){
-            if(umap.find(nums[i])!=umap.end()){
-               md = min(i-umap[nums[i]],md);
-               if(md<=k) return true;
-            }
+            if(umap.find(nums[i])!=umap.end() && i-umap[nums[i]]<=k)
+                return true;
             umap[nums[i]] = i;
         }
         return false;
