ca5e1bec6be26903bf6f9487caba65f05cd79d8d
Zixuan QU <qzx1107@gmail.com>
2015-06-02 22:31:29 +0200

    Search a 2D Matrix

--- Code/searchMatrix.cpp
+++ Code/searchMatrix.cpp
@@ -1,3 +1,18 @@
+//second
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n = matrix.size();
+        if(!n) return false;
+        int m = matrix[0].size();
+        for(auto v:matrix)
+            if(target>=v[0] && target<=v[m-1]&& binary_search(v.begin(),v.end(),target))
+                    return true;
+        return false;
+            
+    }
+};
+//first
 class Solution {
 public:
     bool searchMatrix(vector<vector<int> > &matrix, int target) {
