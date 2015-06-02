f5f9e496989d4cc511553a2e1c9745b72dbed6bf
Zixuan QU <qzx1107@gmail.com>
2015-06-02 04:32:03 +0200

    Remove Duplicates from Sorted List II

--- Code/deleteDuplicatesFromSortedList.cpp
+++ Code/deleteDuplicatesFromSortedList.cpp
@@ -1,31 +1,26 @@
 //iteration
 class Solution {
 public:
-	ListNode *deleteDuplicates(ListNode *head) {
-		if (!head || !head->next)
-			return head;
-		ListNode *root = new ListNode(0), *pre;
-		root->next = head;
-		pre = root;
-		while (head->next) {
-			if (head->val == head->next->val) {
-				int mark = head->val;
-				while (head && head->val == mark) {
-					ListNode *tmp = head;
-					head = head->next;
-					pre->next = head;
-					delete tmp;
-				}
-				if (!head)
-					return root->next;
-			} else {
-				pre = pre->next;
-				head = head->next;
-
-			}
-		}
-		return root->next;
-	}
+    ListNode *deleteDuplicates(ListNode *head) {
+        if(!head||!head->next) return head;
+        ListNode *root = new ListNode(0),*pre=root;
+        root->next = head;
+        while(head->next){
+            if(head->val == head->next->val){
+                int mark = head->val;
+                while(head && head->val == mark){
+                    pre->next = head->next;
+                    delete head;
+                    head = pre->next;
+                }
+                if(!head) return root->next;
+            }else {
+                pre=head;
+                head = head->next;
+            }
+        }
+        return root->next;
+    }
 };
 //recursion
 class Solution {
