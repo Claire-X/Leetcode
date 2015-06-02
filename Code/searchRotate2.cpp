c17834f43406b5a076c8a999345b4d5889bb248e
Zixuan QU <qzx1107@gmail.com>
2015-06-02 17:45:30 +0200

    Search in Rotated Sorted Array II

--- Code/searchRotate2.cpp
+++ Code/searchRotate2.cpp
@@ -1,3 +1,27 @@
+//second
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int left = 0, right = nums.size()-1;
+        while(left<=right){
+            int mid = (left+right)/2;
+            if(nums[mid]==target) return true;
+            if(nums[mid]<nums[left]){
+                if(target>nums[mid]&&target<=nums[right])
+                    left=mid+1;
+                else right = mid-1;
+            }
+            else if(nums[mid]>nums[left]){
+                if(target>=nums[left]&&target<nums[mid])
+                    right = mid-1;
+                else left = mid+1;
+            }else left++; //[1,1,3,1] [1,3,1,1,1] 无法分辨target所在区间，因此简单增加left
+                        //考虑只剩两个元素的情况只能是left++不能right--
+        }
+        return false;
+    }
+};
+//first
 class Solution {
 public:
     bool search(int A[], int n, int target) {
