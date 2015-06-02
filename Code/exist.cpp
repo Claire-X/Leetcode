c020dbd7dfa4282af932aa61e75c49933ea412d4
Zixuan QU <qzx1107@gmail.com>
2015-06-02 19:13:56 +0200

    Word Search

--- Code/exist.cpp
+++ Code/exist.cpp
@@ -1,3 +1,31 @@
+//second
+class Solution {
+public:
+    bool exist(vector<vector<char>>& board, string word) {
+        bool flag;
+        for(int i =0;i<board.size();i++)
+            for(int j=0;j<board[0].size();j++)
+                if(board[i][j]==word[0] && dfs(board,i,j,0,word))
+                        return true;
+    }
+    
+    bool dfs(vector<vector<char>>& board, int i,int j,int step, string word){
+        if(step == word.size()) return true;
+        if(i<0||i>=board.size()||j<0||j>=board[0].size())
+            return false;
+        
+        if(board[i][j]!=word[step]) return false;
+        board[i][j]='*';
+        if(dfs(board,i+1,j,step+1,word)||dfs(board,i-1,j,step+1,word)||dfs(board,i,j+1,step+1,word)||dfs(board,i,j-1,step+1,word))
+            {   board[i][j]=word[step];
+                return true;
+            }
+        board[i][j]=word[step];
+        return false;
+    }
+};
+
+//first
 class Solution {
 public:
     bool exist(vector<vector<char> > &board, string word) {
