a878ae51e1d7b968c2b13c265ab334c8ce9882b9
Zixuan QU <qzx1107@gmail.com>
2015-06-02 17:07:36 +0200

    Search in Rotated Sorted Array

--- Code/searchRotate.cpp
+++ Code/searchRotate.cpp
@@ -1,3 +1,29 @@
+//second : mid只可能与left相等 不可能与right 相等
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0, right = nums.size()-1;
+        while(left<=right){
+            int mid = (left+right)/2;
+            if(nums[mid]==target) return mid;
+            if(nums[mid]<nums[left]){
+                if(target>nums[mid]&&target<=nums[right])
+                    left=mid+1;
+                else right = mid-1;
+            }
+            else{
+                if(target>=nums[left]&&target<nums[mid])
+                    right = mid-1;
+                else left = mid+1;
+            }  
+            
+        }
+        return -1;
+    }
+};
+
+//first
 class Solution {
 public:
     int search(int A[], int n, int target) {
