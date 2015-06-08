50359492e8c5f995e5f011b0bc976eea157fd65d
Zixuan QU <qzx1107@gmail.com>
2015-06-08 15:19:35 +0200

    Count Complete Tree Nodes

---
+++ Code/countNodes.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int countNodes(TreeNode* root) {
+        if(!root) return 0;
+        TreeNode* ls=root, *rs=root;
+        int l=0,r=0;
+        while(ls){
+            ls = ls->left;
+            l++;
+        }
+        while(rs){
+            rs=rs->right;
+            r++;
+        }
+        if(r==l) return pow(2,l)-1;
+        return 1+countNodes(root->left)+countNodes(root->right); 
+    }
+    
+};
