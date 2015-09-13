0e095bef98936a6ed5867541c2fa2e132b7ca273
Zixuan QU <qzx1107@gmail.com>
2015-09-13 05:18:36 +0200

    Update hIndex.cpp

--- Code/hIndex.cpp
+++ Code/hIndex.cpp
@@ -1,4 +1,21 @@
-//binary search
+//n
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        int n = citations.size(),h=0;
+        int* radix = new int[n+1]();
+        
+        for(int c:citations)
+            radix[min(c,n)]++;
+            
+        for(int i=n;i;i--){
+            h+=radix[i];
+            if(h>=i) return i;
+        }
+        
+    }
+};
+//binary search nlogn
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
