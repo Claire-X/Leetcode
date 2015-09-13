cd3a261c96d59198cec774edc77db05b663a3d1a
Zixuan QU <qzx1107@gmail.com>
2015-09-13 06:56:44 +0200

    Best Time to Buy and Sell Stock II

--- Code/maxProfit.cpp
+++ Code/maxProfit.cpp
@@ -1,3 +1,15 @@
+//third time
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit=0;
+        for(int i=1;i<prices.size();i++){
+            profit+=max(0,prices[i]-prices[i-1]);
+        }
+        return profit;
+    }
+};
+//first time
 class Solution {
 public:
 	int maxProfit(vector<int> &prices) {
