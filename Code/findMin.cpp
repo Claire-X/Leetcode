3c063e9bfb9ec6342739b46a741f49b7ff62079f
Zixuan QU <qzx1107@gmail.com>
2015-09-14 10:49:39 +0200

    Find Minimum in Rotated Sorted Array

--- Code/findMin.cpp
+++ Code/findMin.cpp
@@ -1,3 +1,39 @@
+//Third Time
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int n = nums.size();
+        int l=0,r=n-1;
+        while(l<r){
+            if(nums[l]<nums[r]) return nums[l];
+            int mid = l+(r-l)/2;
+            if(nums[mid]<nums[l]) r = mid;
+            else l = mid+1;
+        }
+        return nums[l];
+    }
+};
+
+// second time write
+class Solution {
+public:
+    int findMin(vector<int> &num) {
+        int first=0, last=num.size()-1,minV = INT_MAX;
+        while(first!=last){
+            int mid = (first+last)/2;
+            if(num[first]>num[mid]) last = mid;
+            else {
+                minV = min(minV,num[first];
+                first = mid+1;
+            }
+        }
+        
+        minV=min(minV,num[first]);
+        return minV;
+    }
+};
+
+//First
 class Solution {
 public:
 	int findMin(vector<int> &num) {
@@ -23,21 +59,3 @@ public:
 	}
 };
 
-// second time write
-class Solution {
-public:
-    int findMin(vector<int> &num) {
-        int first=0, last=num.size()-1,minV = INT_MAX;
-        while(first!=last){
-            int mid = (first+last)/2;
-            if(num[first]>num[mid]) last = mid;
-            else {
-                minV = min(minV,num[first];
-                first = mid+1;
-            }
-        }
-        
-        minV=min(minV,num[first]);
-        return minV;
-    }
-};
