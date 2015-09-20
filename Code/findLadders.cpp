30f7879466a9d61ed5622b3630663f21376b68f4
Zixuan QU <qzx1107@gmail.com>
2015-09-20 05:48:53 +0200

    Word Ladder II

--- Code/findLadders.cpp
+++ Code/findLadders.cpp
@@ -1,3 +1,54 @@
+ //Used vector<string> for cur/next in my original answer, but output limit exceeded, guess unordered_set is smaller in matter of space
+  class Solution {
+      public:
+          vector<vector<string>> findLadders(string beginWord, string endWord, unordered_set<string> &wordList) {
+              unordered_set<string> cur,next;
+              vector<vector<string>> res;
+
+              unordered_map<string,vector<string> > tree;
+
+              cur.insert(beginWord);
+              int l=beginWord.length();
+              bool flag = false;
+
+              wordList.insert(endWord);
+              while(cur.size() && !flag){
+                  for(string w:cur){
+                  	string s = w;
+                      for(int i =0;i<l;i++)
+                          for(char c ='a';c<='z';c++){
+                              if(c == s[i]) continue;
+                              swap(c,s[i]);
+                              if (s == endWord||wordList.find(s)!=wordList.end()){
+                                  tree[s].push_back(w);
+                                  next.insert(s);
+                                  if(s==endWord) flag = true;
+                                 }
+                              swap(c,s[i]);
+                          }
+                  }
+                  cur.clear();
+                  swap(cur,next);
+                  for(string s:cur) wordList.erase(s);
+             }
+             vector<string> path;
+             if(flag) buildPath(endWord,beginWord,tree,path,res);
+
+              return res;
+          }
+
+          void buildPath(string &bs, string &es, unordered_map<string,vector<string> > &tree, vector<string> &path, vector<vector<string>> &res){
+        	        path.push_back(bs);
+        	        cout<<bs<<"   ";
+                    if(bs==es){
+                        res.push_back(path);
+                        reverse(res.back().begin(),res.back().end());
+                    }else for(string s: tree[bs])
+                        buildPath(s,es,tree,path,res);
+                    path.pop_back();
+           }
+};
+
 //Time Limit Exceeded
 class Solution {
 public:
