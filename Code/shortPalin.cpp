aa0e2ddf027f381300b7367c9ebb83a4db1fd5fe
Zixuan QU <qzx1107@gmail.com>
2015-09-21 13:09:58 +0200

    Shortest Palindrome

--- Code/shortPalin.cpp
+++ Code/shortPalin.cpp
@@ -1,3 +1,18 @@
+//second time
+class Solution {
+public:
+    string shortestPalindrome(string s) {
+        int n = s.size();
+        if(n==0) return string("");
+        string ss(s);
+        reverse(ss.begin(),ss.end());
+        for(int i=0;i<n;i++){
+            if(s.substr(0,n-i) == ss.substr(i))//find out the longest palindrome in s  
+                return ss.substr(0,i)+s;//只能在s前面添加字母，则所找的回文必须开始于s[0];
+        }
+    }
+};
+//first
 class Solution {
 public:
     string shortestPalindrome(string s) {
